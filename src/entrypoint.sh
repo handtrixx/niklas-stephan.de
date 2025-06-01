cac5aeb3c2e5520cc80ae7e5818e73a7ab5e7047
handtrixx <pooh85@gmx.de>
2025-06-01 19:55:23 +0200

    ignoring dist in git

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #cleanup dist folder
 rm -rf /hugo/dist/*
+hugo --environment production --destination /hugo/dist --baseURL $URL --minify
 hugo server --environment development --bind 0.0.0.0 --baseURL http://localhost
 tail -f /dev/null
