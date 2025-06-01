3dcfac8c2a0db490fa4f0438417e0b54a8559b6e
handtrixx <pooh85@gmx.de>
2025-06-01 20:10:07 +0200

    distinguish prd and dev

--- src/cron.sh
+++ src/cron.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-cd /hugo
-hugo --environment production --destination /hugo/dist --baseURL $URL --minify
+
+if [ -n "$ENVIRONMENT" ]; then
+    echo "Environment variable is set to: $ENVIRONMENT"
+else
+    echo "Environment variable is not set"
+    $ENVIRONMENT=production
+    echo "Environment variable is now set to: $ENVIRONMENT"
+fi
+if [ -n "$URL" ]; then
+    echo "URL variable is set to: $URL"
+else
+    echo "URL variable is not set"
+    $URL=https://niklas-stephan.de
+    echo "URL variable is now set to: $URL"
+fi
+if [ "$ENVIRONMENT" = "production" ]; then
+    cd /hugo
+    hugo --environment production --destination /hugo/dist --cleanDestinationDir --baseURL $URL --minify
+fi
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
+if [ -n "$ENVIRONMENT" ]; then
+    echo "Environment variable is set to: $ENVIRONMENT"
+else
+    echo "Environment variable is not set"
+    $ENVIRONMENT=production
+    echo "Environment variable is now set to: $ENVIRONMENT"
+fi
+if [ -n "$URL" ]; then
+    echo "URL variable is set to: $URL"
+else
+    echo "URL variable is not set"
+    $URL=https://niklas-stephan.de
+    echo "URL variable is now set to: $URL"
+fi
 #cleanup dist folder
-rm -rf /hugo/dist/*
-hugo --environment production --destination /hugo/dist --baseURL $URL --minify
-hugo server --environment development --bind 0.0.0.0 --baseURL http://localhost
+if [ "$ENVIRONMENT" = "production" ]; then
+    rm -rf /hugo/dist/*
+    hugo --environment production --destination /hugo/dist --baseURL $URL --minify
+    
+else 
+    hugo server --environment development --bind 0.0.0.0 --baseURL http://localhost
+fi
 tail -f /dev/null
+
+
